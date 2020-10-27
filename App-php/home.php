b2bebddb3b13a670f9a1648e2771370d43243cae
sermeor <sergiomenaortega95@gmail.com>
2020-10-27 09:48:57 +0000

    Change of name of application

--- App-php/home.php
+++ App-php/home.php
@@ -61,7 +61,7 @@ $(".se-pre-con").fadeOut("slow");
 <nav class="w3-sidebar w3-indigo w3-collapse w3-top w3-large w3-padding" style="z-index:3;width:300px;font-weight:bold;" id="mySidebar"><br>
 <a href="javascript:void(0)" onclick="w3_close()" class="w3-button w3-hide-large w3-display-topleft" style="width:100%;font-size:22px">Close Menu</a>
 <div class="w3-container">
-<h3 class="w3-padding-64"><b>FSCV<br>Datalab</b></h3>
+<h3 class="w3-padding-64"><b>Analysis<br>Kid</b></h3>
 </div>
 <div class="w3-bar-block">
 <a href="#top" onclick="w3_close()" class="w3-bar-item w3-button w3-hover-white"><i class="fa fa-home w3-large"></i> Home</a>
