7d32914d3e5ef112818eb12a799e11cfb0d1bec5
sermeor <51787515+sermeor@users.noreply.github.com>
2021-05-12 10:09:40 +0100

    Fixed error on simple gaussian blur.

--- JavaScriptPackages/ArrayMethods.js
+++ JavaScriptPackages/ArrayMethods.js
@@ -371,24 +371,24 @@ return sizes;
 //Traditional definition of Gaussian convolution.
 
 
-  function simple_gauss_blur(scl, tcl, w, h, r) {
-  var rs = Math.ceil(r * 2.57);
-  // significant radius
-  for(var i=0; i<h; i++)
-  for(var j=0; j<w; j++) {
-  var val = 0, wsum = 0;
-  for(var iy = i-rs; iy<i+rs+1; iy++)
-  for(var ix = j-rs; ix<j+rs+1; ix++) {
-  var x = Math.min(w-1, Math.max(0, ix));
-  var y = Math.min(h-1, Math.max(0, iy));
-  var dsq = (ix-j)*(ix-j)+(iy-i)*(iy-i);
-  var wght = Math.exp( -dsq / (2*r*r) ) / (Math.PI*2*r*r);
-  val += scl[y*w+x] * wght;
-  wsum += wght;
-  }
-  tcl[i*w+j] = Math.round(val/wsum);
-  }
-  };
+function simple_gauss_blur(scl, tcl, w, h, r) {
+var rs = Math.ceil(r * 2.57);
+// significant radius
+for(var i=0; i<h; i++)
+for(var j=0; j<w; j++) {
+var val = 0, wsum = 0;
+for(var iy = i-rs; iy<i+rs+1; iy++)
+for(var ix = j-rs; ix<j+rs+1; ix++) {
+var x = Math.min(w-1, Math.max(0, ix));
+var y = Math.min(h-1, Math.max(0, iy));
+var dsq = (ix-j)*(ix-j)+(iy-i)*(iy-i);
+var wght = Math.exp( -dsq / (2*r*r) ) / (Math.PI*2*r*r);
+val += scl[y*w+x] * wght;
+wsum += wght;
+}
+tcl[i*w+j] = val/wsum;
+}
+};
 
 // Functions for complex numbers.
 function Complex(real, imaginary) {
