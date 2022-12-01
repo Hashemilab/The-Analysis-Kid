1d9959a1258853c19232b3709b907adaf2c3f3e9
sermeor <51787515+sermeor@users.noreply.github.com>
2022-12-01 16:27:23 +0000

    Changes for Firebase
    Changes for holding the website in firebase.

--- Python/test.py
+++
@@ -1,5 +0,0 @@
-import json
-print(json.dumps('Python test'))
-#the script can be called from PHP:
-#alert("<?php echo json_decode(shell_exec(escapeshellcmd('python3 test.py')))?>");
-#AJAX calls can also be used
